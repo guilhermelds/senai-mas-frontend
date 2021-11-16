153fc6345dd3e88cb0adfd32f3d6396c4c9c8c1a
Guilherme Lenon da Silva <gulhermelds@gmail.com>
2021-11-16 20:03:07 -0300

    add config para deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+  baseURL: process.env.NODE_ENV === 'production' 
+    ?'https://mas-beckend-senai.herokuapp.com'
+    :'http://localhost:3333',
 });
 
 export default api;
